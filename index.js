7f46e3696df5721325032a8ba77ca2accd68b383
Santy <160811397+jesudavix@users.noreply.github.com>
2024-02-21 22:56:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Te quero')
+    alert('TE AMO 💗')
 });
 
 const noBtn = document.querySelector('#noBtn');
